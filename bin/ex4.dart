1c67043c0c711f8162ab44cdbf24c4f55e89a1f6
Joao Minanti <joaovbminanti@gmail.com>
2025-06-01 15:12:49 -0300

    Ex4 implementado.

---
+++ bin/ex4.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main() {
+    
+  // Fahrenheit = (Celsius * 9/5) + 32.
+  // Celsius = (Fahrenheit - 32) * 5/9.
+  const double fatorSomaSubstracao = 32;
+  const double fatorMultiplicacaoFahrenheit = 9/5;
+  const double fatorMultiplicacaoCelsius = 5/9;
+
+  // Define Variáveis
+  double temperaturaCelsiusOriginal = 10.0;
+  final double temperaturaConvertidaFahrenheit;
+  final double temperaturaReconvertidaCelsius;
+
+  // Converte
+  temperaturaConvertidaFahrenheit = (temperaturaCelsiusOriginal * fatorMultiplicacaoFahrenheit) + fatorSomaSubstracao;
+  temperaturaReconvertidaCelsius = (temperaturaConvertidaFahrenheit - fatorSomaSubstracao) * fatorMultiplicacaoCelsius;
+
+  imprimirConversoes(temperaturaCelsiusOriginal, temperaturaConvertidaFahrenheit, temperaturaReconvertidaCelsius);
+
+}
+
+imprimirConversoes(double temperaturaCelsiusOriginal, double temperaturaConvertidaFahrenheit, double temperaturaReconvertidaCelsius) {
+  print("Temperatura Celsius Original: $temperaturaCelsiusOriginal°C");
+  print("Convertido para Fahrenheit: ${temperaturaConvertidaFahrenheit.toStringAsFixed(2)}°F");
+  print("Reconvertido para Celsius: ${temperaturaReconvertidaCelsius.toStringAsFixed(2)}°C");
+}
